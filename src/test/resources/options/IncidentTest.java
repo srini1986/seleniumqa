a566ed2348f768ed5d27cc31e3e5f558de192dd1
ganeshanupala <42455389+ganeshanupala@users.noreply.github.com>
2019-09-11 10:44:41 +0530

    Update IncidentTest.java

--- src/test/resources/options/IncidentTest.java
+++ src/test/resources/options/IncidentTest.java
@@ -8,6 +8,6 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		plugin = {"pretty"},
-		glue = {"C:\\Users\\sdhar\\Downloads\\942019\\NewCode\\src\\test\\java\\stepDef\\CreateIncidentDefinitions.java"},
-		features = {"C:\\Users\\sdhar\\Downloads\\942019\\NewCode\\src\\test\\resources\\Feature\\CreateIncident.feature"})
+		glue = {"src/test/java/stepDef/CreateIncidentDefinitions.java"},
+		features = {"src/test/resources/Feature/CreateIncident.feature"})
 public class IncidentTest {}
